139c766c030276a3f6a9627cd54482ad1b53e4b3
Abhra Pal <palabhra123@gmail.com>
2023-05-13 18:10:24 +0530

    space remove

--- src/App.test.js
+++ src/App.test.js
@@ -3,14 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('avro testing', () => {
   render(<App />);
-
   const linkElement = screen.getByText(/I am abhra/i);
   expect(linkElement).toBeInTheDocument();
 });
